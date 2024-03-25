e363fe5225e14c53b6dd6fcd04fce7028a92e5d9
monserrj <j.monserratrelano@lancaster.ac.uk>
2024-03-25 15:46:07 +0000

    Comments to skeleton sq file with doubts and explanations added

--- scripts/BMC-database-skeleton.sql
+++ scripts/BMC-database-skeleton.sql
@@ -1,4 +1,7 @@
-.mode column.header on CREATE TABLE Protein (
+.mode column
+.header on
+
+CREATE TABLE Protein (
     prot_ID integer,
     prot_seq integer,
     locus_NCBI_ID varchar not null,
@@ -13,25 +16,25 @@ CREATE TABLE Gene (
     primary key(gene_ID)
 );
 
--- to make a protein:gene relationship primary (promote it), then:
--- 1. move primary relationship to name_rank of current largest number + 1
--- 2. mover promoted gene to have name_rank 1
 CREATE TABLE Protein_gene (
     prot_ID integer,
     gene_ID integer,
-    --primary_name boolean,
     name_rank integer,
-    -- primary gene gets rank 1; other names get 2, 3, ...
+    -- primary gene gets rank 1; other names get 2, 3, ... Changed from bolean to be able to use UNIQUE
+    -- to make a protein:gene relationship primary (promote it), then:
+    -- 1. move primary relationship to name_rank of current largest number + 1
+    -- 2. mover promoted gene to have name_rank 1
     foreign key(prot_ID) references Protein(prot_ID),
     foreign key(gene_ID) references Gene(gene_ID),
     UNIQUE(prot_id, gene_ID, name_rank)
+    -- to make sure no protein has more than one main name
 );
 
 CREATE TABLE Taxon (
     tax_ID integer,
     tax_ref varchar not null,
-    -- could be NCBI or GTDB
-    taxdb varchar not null,
+    -- ref number in remote db
+    tax_db varchar not null,
     -- stores whether NCBI or GTDB
     specie integer,
     genus integer,
@@ -51,27 +54,27 @@ CREATE TABLE Protein_taxon (
 );
 
 CREATE TABLE Pdb (
+-- if empty alphafold or other software will be used to predict structure
     pdb_ID integer,
     pdb_rscb_acc_1 varchar,
     pdb_rscb_acc_2 varchar,
-    -- primary_pdb boolean,
+    -- primary_pdb boolean. It does not matter which one is primary. Not needed
     primary key(pdb_ID)
 );
 
 CREATE TABLE Protein_pdb (
     pdb_ID integer,
     prot_ID integer,
-    -- primary_pdb boolean,
     foreign key(prot_ID) references Protein(prot_ID),
     foreign key(pdb_ID) references Pdb(pdb_ID)
 );
 
 CREATE TABLE Domain (
     domain_ID integer,
-    -- domain accession in remote db
     dom_ref_fam varchar not null,
+    -- domain accession in remote db
     ext_source integer not null,
-    -- doman database
+    -- domain database, such as pfam, CDD...
     primary key(domain_ID)
 );
 
@@ -85,43 +88,19 @@ CREATE TABLE Protein_domain (
 CREATE TABLE ISOFORMS (
     canonical_prot_ID integer,
     isoform_prot_ID integer,
+    -- Isoform ID is related to those proteins that may have to RBD
+    -- 2 proteins from he same DNA seq, such as pduB and pduB'
     foreign key(canonical_prot_ID) references Protein(prot_ID),
     foreign key(isoform_prot_ID) references Protein(prot_ID)
 );
 
-/*
- CREATE TABLE Function_MF (
- MF_ID integer,
- GO_MF_1 varchar,
- description_MF_1 integer,
- GO_MF_2 varchar,
- description_MF_2 integer,
- primary key(MF_ID)
- );
- 
- CREATE TABLE Function_CC (
- CC_ID integer,
- GO_CC_1 varchar,
- description_CC_1 integer,
- GO_CC_2 varchar,
- description_CC_2 integer,
- primary key(CC_ID)
- );
- 
- CREATE TABLE Function_BP (
- BP_ID integer,
- GO_BP_1 varchar,
- description_BP_1 integer,
- GO_BP_2 varchar,
- description_BP_2 integer,
- primary key(BP_ID)
- );
- */
 CREATE TABLE Function_GO (
     goID integer,
+    -- ref number in GO db
     go_type varchar,
-    -- MF, CC, or BP
+    -- MF (molecular function), CC (cellular compartment), or BP (biological process)
     go_description varchar,
+    -- text description of the function
     primary key(goID)
 );
 
@@ -135,6 +114,7 @@ CREATE TABLE Protein_GO (
 CREATE TABLE Enzyme_path (
     path_ID integer,
     KO varchar,
+    -- ref number from Kegg Ontology db
     primary key(path_ID)
 );
 
@@ -144,35 +124,30 @@ CREATE TABLE Protein_Path (
     foreign key(path_ID) references Enzyme_path(path_ID),
     foreign key(protein_ID) references Protein(prot_ID),
 )
-/*
- CREATE TABLE Protein_function (
- BP_ID integer,
- CC_ID integer,
- MF_ID integer,
- path_ID integer,
- prot_ID integer,
- foreign key(BP_ID) references Function_BP(BP_ID),
- foreign key(CC_ID) references Function_CC(CC_ID),
- foreign key(MF_ID) references Function_MF(MF_ID),
- foreign key(path_ID) references Enzyme_path(path_ID),
- foreign key(prot_ID) references Protein(prot_ID)
- );
- */
+
 CREATE TABLE Complex (
     complex_ID integer,
+    -- can be the native complex, e.g.: pdu operon, or a synthetic one, e.g.: pduABJMNK
     complex_type integer,
-    complex_activity,
+    -- need to decide which classification to use. Axen paper?
+    complex_activity integer,
+    -- Active/inactice
     assembly_exp_tested integer,
+    -- Is the assembly of this compartment experimentally tested (Y/N)?
+    -- Consider whether if Y answer paper references should be added. How?
     primary key(complex_ID)
 );
 
 CREATE TABLE Protein_complex(
     complex_ID integer,
+    -- Several proteins can belong to the same complex and one protein to several complexes
     prot_ID integer,
-    prot_essential_assembly integer,
-    -- prot_interact_ID integer,
-    copy_number integer,
-    structural_prot_type integer,
+    prot_essential_assembly integer, -- is this protein essential for the assembly of the complex (Y/N)?
+    
+    -- prot_interact_ID integer (ask Leighton, is about direct interation not complex)
+    
+    copy_number integer, -- protein copy number in complex (stecheometry)
+    structural_prot_type integer, -- to define if it is an hexamer, pentamer, pseudohexamer (trimer) or not structural
     foreign key(complex_ID) references Complex(complex_ID),
     foreign key(prot_ID) references Protein(prot_ID),
     -- foreign key(prot_interact_ID) references Protein(prot_ID)
